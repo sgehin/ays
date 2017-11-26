82d5be55b16fc294c47be002041837152515123e
sgehin <sfa.gehin@gmail.com>
2017-11-26 16:49:28 +0100

    notes is not saved when empty

--- saveNote.php
+++ saveNote.php
@@ -4,17 +4,18 @@ $conn = connectDB();
 // MySQL DATETIME format)
 $myDate = date("Y-m-d H:i:s");
 // checken of data ingevult is
-if (isset($_REQUEST['note'])) {
-    $savecontainer = mysql_fix_string($conn, $_REQUEST['note']);
-//    $savecontainer = $_REQUEST['note'];
-    $sql = "INSERT INTO `note`( `note`, `date_id`)VALUES('$savecontainer','$myDate')";
-    $conn->query($sql);       
-    $conn->close();
-    echo "is opgeslagen";
-    
-    
-}
-
-header("Location: workplace.php");
-
+if( isset($_GET['note']) and ($_GET['note'] == '') ) {
+     echo "No data to save";
+     header("Location: workplace.php");
+}     
+     elseif (isset($_REQUEST['note'])) {
+                $savecontainer = mysql_fix_string($conn, $_REQUEST['note']);
+                $sql = "INSERT INTO `note`( `note`, `date_id`)VALUES('$savecontainer','$myDate')";
+                $conn->query($sql);       
+                $conn->close();
+                echo "is opgeslagen";
+                header("Location: workplace.php");
+     }
+     
+     
 ?>
